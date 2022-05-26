904230c1a042b13292235790fa4bbc59cfe0d826
kadodibg <kadodi@gmail.com>
2022-05-26 19:00:14 +0300

    up

--- Arrays-Exercises/03.mergeArrays.js
+++ Arrays-Exercises/03.mergeArrays.js
@@ -1,13 +1,14 @@
 function mergeArrays(arr1, arr2) {
-    let arr3 = " ";
+    let arr3 = [];
     for (let i = 0; i < arr1.length; i++) {
+        
         if (i % 2 === 0) {
             arr3 += (Number(arr1[i]) + Number(arr2[i]));
         } else {
             arr3 += arr1[i] + arr2[i];
         }
     }
-    console.log(arr3);
+    console.log(arr3.join(' - '));
     // output 22 - 1522 - 110 - 5636 - 46
 }
 mergeArrays(['5', '15', '23', '56', '35'],
