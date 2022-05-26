e020e80d19490f18fe20c5b3d02aa9ff10f23612
kadodibg <kadodi@gmail.com>
2022-05-26 19:42:02 +0300

    up

--- Arrays-Exercises/03.mergeArrays.js
+++ Arrays-Exercises/03.mergeArrays.js
@@ -1,15 +1,16 @@
 function mergeArrays(arr1, arr2) {
     let arr3 = [];
+
     for (let i = 0; i < arr1.length; i++) {
         
-        if (i % 2 === 0) {
-            arr3 += (Number(arr1[i]) + Number(arr2[i]));
+        if (i % 2 == 0) {
+           arr3 += Number(arr1[i]) + Number(arr2[i]);
         } else {
-            arr3 += arr1[i] + arr2[i];
+            arr3 += arr1[i].toString() + arr2[i].toString();
+            //console.log(arr3.join(' - '));
         }
     }
-    console.log(arr3.join(' - '));
-    // output 22 - 1522 - 110 - 5636 - 46
+   // console.log([arr3.join(' - ')]);
+   console.log(arr3.join('-'));
 }
-mergeArrays(['5', '15', '23', '56', '35'],
-    ['17', '22', '87', '36', '11']);
+mergeArrays(['5', '15', '23', '56', '35'], ['17', '22', '87', '36', '11']);
