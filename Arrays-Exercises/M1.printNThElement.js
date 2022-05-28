1212839e9b508d726e90725f82875a41857f8f90
kadodibg <kadodi@gmail.com>
2022-05-29 00:12:58 +0300

    up

--- Arrays-Exercises/M1.printNThElement.js
+++ Arrays-Exercises/M1.printNThElement.js
@@ -1,12 +1,12 @@
-function printNThElement(arr){
-
-    for(let i = 0; i < arr.length; i++){
-        //console.log(arr[i])
-        let constArray = arr.pop(2);
-        // str.charCodeAt(i + 1);
-        console.log(constArray)
-    
-    } 
-
+function printNThElement(arr) {
+    let lastNum = arr.pop();
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (i % lastNum === 0) {
+            newArr.push(arr[i]);
+        }
+    }
+    console.log(newArr.join(' '))
 }
 printNThElement(['5', '20', '31', '4', '20', '2'])
+printNThElement(['dsa', 'asd', 'test', 'test', '2'])
