c6c9150a17b2844035e0408f9da463f8e36ab74e
kadodibg <kadodi@gmail.com>
2022-05-29 07:16:04 +0300

    New

--- Arrays-Exercises/M2.addAndRemove.js
+++ Arrays-Exercises/M2.addAndRemove.js
@@ -1,19 +1,17 @@
 function addAndRemove(arr) {
     let newArr = [];
-
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === 'add') {
             newArr.push([i + 1]);
             } else {
-            newArr.pop([i]);
+            newArr.pop();
                 }
-    }
+            }
     if (newArr.length === 0) {
         console.log("Empty");
         } else {
             console.log(newArr.join(' '));
                 }
-
 }
 addAndRemove(['add', 'add', 'add', 'add']);
 addAndRemove(['add', 'add', 'remove', 'add', 'add']);
