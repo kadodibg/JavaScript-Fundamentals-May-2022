5fb8171d33bc09400031ebe5bd827901a9cd4f33
kadodibg <kadodi@gmail.com>
2022-06-04 05:59:25 +0300

    UP

--- Function/05.Simple Calculator.js
+++ Function/05.Simple Calculator.js
@@ -1,26 +1,24 @@
-function simpleCalculator(a, b, operator){
+function simpleCalculator(a, b, operator) {
     let result = 0;
-    
-    switch(operator){
+    switch (operator) {
         case "multiply":
             let multiply = (a, b) => a * b;
-            result = multiply(a,b);
+            result = multiply(a, b);
             break;
-        case "divide":  
+        case "divide":
             let divide = (a, b) => a / b;
             result = divide(a, b);
             break;
         case "add":
             let add = (a, b) => a + b;
-            result = add (a,b);
+            result = add(a, b);
             break;
         case "subtract":
             let subtract = (a, b) => a - b;
             result = subtract(a, b)
             break;
     }
-
-    console.log(result.toFixed(2))
+    console.log(result)
 }
 simpleCalculator(5, 5, 'multiply');
 simpleCalculator(40, 8, 'divide');
