27f27fce372d21ac0c5b700e4f37eeb6cab4c916
kadodibg <kadodi@gmail.com>
2022-06-07 13:32:55 +0300

    UP

--- Arrays-Advanced-Lab/solve.js
+++ Arrays-Advanced-Lab/solve.js
@@ -13,28 +13,31 @@
 // myArrays.includes('George');
 // myArrays.includes('John');
 
-let arr = [5, 3, 8, -2, 11, -23];
-console.log(myIncludes(arr, 3));
-console.log(myIncludes(arr, 4));
-console.log(myIncludes(arr, -23));
+let arr = [-23, 5, 3, 8, -2, 11,];
+arr.unshift(arr.pop());
+console.log(arr[4]);
 
-console.log(myIndexOf(arr, 3));
-console.log(myIndexOf(arr, 4));
-console.log(myIndexOf(arr, -23));
+// console.log(myIncludes(arr, 3));
+// console.log(myIncludes(arr, 4));
+// console.log(myIncludes(arr, -23));
 
-function myIncludes(arr, value) {
-    for (let element of arr) {
-        if (element == value) {
-            return true;
-        }
-    }
-}
+// console.log(myIndexOf(arr, 3));
+// console.log(myIndexOf(arr, 4));
+// console.log(myIndexOf(arr, -23));
 
-function myIndexOf(arr, value) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == value) {
-            return i;
-        }
-    }
-    return -1;
-}
+// function myIncludes(arr, value) {
+//     for (let element of arr) {
+//         if (element == value) {
+//             return true;
+//         }
+//     }
+// }
+
+// function myIndexOf(arr, value) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] == value) {
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
