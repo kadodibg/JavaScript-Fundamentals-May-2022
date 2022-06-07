7dcd5f48f4157a1ff0b1eb8bfa26ba8d3080aba5
kadodibg <kadodi@gmail.com>
2022-06-07 15:04:44 +0300

    New&UP

--- Arrays-Advanced-Lab/solve.js
+++ Arrays-Advanced-Lab/solve.js
@@ -13,9 +13,42 @@
 // myArrays.includes('George');
 // myArrays.includes('John');
 
-let arr = [-23, 5, 3, 8, -2, 11,];
-arr.unshift(arr.pop());
-console.log(arr[4]);
+// let arr = [-23, 5, 3, 8, -2, 11,];
+// arr.unshift(arr.pop());
+// console.log(arr[4]);
+let input = [5, 3, 8, -2, 11, -23];
+let r1 = mySlice(input, -4, -2);
+let r2 = input.slice(-4, -2);
+console.log(r1, r2)
+
+function mySlice(arr, start, end){
+    let result = [];
+   
+    if(end == undefined){
+        end = arr.length
+    }
+
+    if(start == undefined){
+        start = arr.length;
+    }
+   
+   
+    if(start < 0 ){
+        start += arr.length
+
+    }
+    if(end < 0 ){
+        start += arr.length
+
+    }
+
+    for(let i = start; i < (end && i < arr.length) ; i++){
+        result.push(arr[i]);
+    }
+    return result;
+}
+
+
 
 // console.log(myIncludes(arr, 3));
 // console.log(myIncludes(arr, 4));
---
+++ Arrays-Advanced-Lab/solve2.js
@@ -0,0 +1,7 @@
+let arr = [10, 20, 30, 40, 50, 60, 70];
+let r = arr.splice(3, 0, "new", "new")
+
+
+console.log('Array', arr);
+console.log('Splice', r)
+
