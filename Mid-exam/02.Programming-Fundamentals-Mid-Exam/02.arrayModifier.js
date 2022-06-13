c456a5239b8a3926ea61d63ee91e5467c392d4f2
kadodibg <kadodi@gmail.com>
2022-06-13 16:33:07 +0300

    Solved

--- Mid-exam/02.Programming-Fundamentals-Mid-Exam/02.arrayModifier.js
+++ Mid-exam/02.Programming-Fundamentals-Mid-Exam/02.arrayModifier.js
@@ -1,15 +1,24 @@
-function arrayModifier(arr){
-    let array = arr.shift().split(' ').map(Number);
-    let command = arr.shift();
+function arrayModifier(arr) {
+    let array = arr.shift().split(' ');
+    let command = arr.shift().split(' ');
+    while (command !== 'end') {
+        let el = command[1];
+        let el1 = command[2];
+        if (command[0] == 'swap') {
+            [array[el], array[el1]] = [array[el1], array[el]];
 
-    while(command != 'end' ){
-    let tokens = command.split(' ');
-
-    console.log(tokens[0])
- 
-   
-    command = arr.shift();
+        } else if (command[0] == 'multiply') {
+            array[el] *= array[el1];
+        } else if (command[0] == 'decrease') {
+            for (let i = 0; i < array.length; i++) {
+                array[i] -= 1;
+            }
+        }
+        if (command == 'end') {
+            break;
+        }
+        command = arr.shift().split(' ');
     }
-
+    console.log(array.join(', '))
 }
-arrayModifier(['23 -2 321 87 42 90 -123', 'swap 1 3', 'swap 3 6', 'swap 1 0', 'multiply 1 2', 'multiply 2 1', 'decrease', 'end']);
+arrayModifier(['23 -2 321 87 42 90 -123', 'swap 1 3', 'swap 3 6', 'swap 1 0', 'multiply 1 2', 'multiply 2 1', 'decrease','pesho', 'end']);
