2fa68bd93e2907dfd54f084e6778eb2ed7f19324
kadodibg <kadodi@gmail.com>
2022-06-21 04:16:06 +0300

    UP

--- Mid-exam/02.Programming-Fundamentals-Mid-Exam/03.Numbers.js
+++ Mid-exam/02.Programming-Fundamentals-Mid-Exam/03.Numbers.js
@@ -1,34 +1,113 @@
-function x(input = String) {
-    let betterInput = input.split(' ').map(Number);
-    let avg = 0;
-    for (let i = 0; i < betterInput.length; i++) {
-        avg += betterInput[i];
-    }
-    let finalSum = avg / betterInput.length;
 
-    let newAvg = [];
-
-    for (let i = 0; i < betterInput.length; i++) {
-        if (finalSum < betterInput[i]) {
-            newAvg.push(betterInput[i])
+function izpit(arr){
+    let inventory = arr.shift().split(', ');
+    let allCommand = [...arr];
+    let index = 0
+    let command = allCommand[index];
+    index++;
+ 
+ 
+    console.log(allCommand);
+    //console.log(inventory)
+ 
+    while (command !== 'Craft!'){
+        let currentCommandLine = command.split(' - ');
+        let currentCommand = currentCommandLine[0];
+        let currentItem = currentCommandLine[1];
+        if (currentCommand == 'Collect'){
+            if (inventory.includes(currentItem)){
+                command = allCommand[index];
+                index++;
+                continue;
+            } else {
+                inventory.push(currentItem);
+            }
+        } else if (currentCommand == 'Drop'){
+            if (inventory.includes(currentItem)){
+                let indexOfItemToDrop = inventory.indexOf(currentItem);
+                inventory.splice(indexOfItemToDrop, 1);
+            } else {
+                command = allCommand[index];
+                index++;
+                continue;
+            }
+        } else if (currentCommand == 'Combine'){
+            currentCommandLine = command.split(' - ');
+            let ElForCombine = currentCommandLine[1].split(':');
+            if (inventory.includes(ElForCombine[0])){
+                let indexOfOldNumber = inventory.indexOf(ElForCombine[0]);
+                inventory.splice(indexOfOldNumber, 0, ElForCombine[1]);
+            } else {
+                command = allCommand[index];
+                index++;
+                continue;
+            }
+        } else {
+            if (inventory.includes(currentItem)){
+                let indexOfItemToMove = inventory.indexOf(currentItem);
+                inventory.splice(indexOfItemToMove, 1);
+                inventory.push(currentItem)
+            }
         }
+        command = allCommand[index];
+        index++;
     }
-    
-    if (newAvg.length <= 0) {
-        console.log('No');
-    }
-    if (newAvg.length > 5) {
-        let go = newAvg.sort((a, b) => b - a);
-        let sliced = go.splice(0, 5);
-        console.log(sliced.join(' '));
-
-    }
-    
-    else if (newAvg.length < 5 && newAvg.length !== 0) {
-        let sorted = newAvg.sort((a, b) => b - a);
-        console.log(sorted.join(' '));
-    } 
-
+   // console.log(inventory)
 }
+izpit([ 
+    'Iron, Sword, Gold',  
+    'Renew - Sword', 
+    'Craft!' 
+  ])
 
-x("10", "20", "30", "40", "50");
+// function numbers (sequence){
+//     let arrOfStrings = sequence.split('');
+    
+//     let arrOfNumbers = [];
+//     let sum = 0;
+
+//     for(let el of arrOfStrings){
+//         let currNum = Number(el);
+//         arrOfNumbers.push(currNum);
+//         sum += currNum
+//     }
+
+//     let avg = sum / arrOfNumbers.length
+//     console.log(avg)
+
+// }
+// numbers('10 20 30 40 50')
+// function x(input = String) {
+//     let betterInput = input.split(' ').map(Number);
+//     let avg = 0;
+//     for (let i = 0; i < betterInput.length; i++) {
+//         avg += betterInput[i];
+//     }
+//     let finalSum = avg / betterInput.length;
+
+//     let newAvg = [];
+
+//     for (let i = 0; i < betterInput.length; i++) {
+//         if (finalSum < betterInput[i]) {
+//             newAvg.push(betterInput[i])
+//         }
+//     }
+    
+//     if (newAvg.length <= 0) {
+//         console.log('No');
+//     }
+//     if (newAvg.length > 5) {
+//         let go = newAvg.sort((a, b) => b - a);
+//         let sliced = go.splice(0, 5);
+//         console.log(sliced.join(' '));
+
+//     }
+    
+//     else if (newAvg.length < 5 && newAvg.length !== 0) {
+//         let sorted = newAvg.sort((a, b) => b - a);
+//         console.log(sorted.join(' '));
+//     } 
+
+// }
+
+// x("10", "20", "30", "40", "50");
--- Mid-exam/02.Programming-Fundamentals-Mid-Exam/solve.js
+++ Mid-exam/02.Programming-Fundamentals-Mid-Exam/solve.js
@@ -1,86 +1,86 @@
 
-// // let arr = ['23 -2 321 87 42 90 -123']
-// // let r = arr.shift().split(' ');    
+// // // let arr = ['23 -2 321 87 42 90 -123']
+// // // let r = arr.shift().split(' ');    
 
-// // function arrToNumber(arr){
+// // // function arrToNumber(arr){
+// // //     let result = [];
+// // //     for(let element of arr){
+// // //         result.push(Number(element));
+// // //     }
+// // //     return result;
+// // // }
+// // // console.log(r[0])
+
+// // let numsAsString = ['5', '23', '45'];
+// // let nums = [5, 23, 45];
+// // let r = mapToNumber(nums)
+// // console.log(r[0] + r[2]);
+
+// // function mapToNumber(arr){
 // //     let result = [];
-// //     for(let element of arr){
+// //     for( element of arr){
 // //         result.push(Number(element));
 // //     }
+
 // //     return result;
 // // }
-// // console.log(r[0])
 
-// let numsAsString = ['5', '23', '45'];
-// let nums = [5, 23, 45];
-// let r = mapToNumber(nums)
-// console.log(r[0] + r[2]);
+// // function increment(arr){
+    
+    
+// //     return x+1;
+  
+// // }
+// // console.log(x)
+
+
+// // function arrayModifier(arr) {
+
+
+// //     let command = arr.shift().split(' ');
+// //     while (command !== 'end') {
+// //         if (command[0] == 'swap') {
+// //         [r[command[1]], r[command[2]]] = [r[command[2]], r[command[1]]];
+// //         } else if (command[0] == 'multiply'){ 
+// //         r[command[1]] *= r[command[2]];
+// //         } else if (command[0] == 'decrease') 
+// //         for (let i = 0; i < r.length; i++) { 
+// //             r[i] -= 1;
+// //         }
+// //             if (command == 'end') { break; }
+// //             command = arr.shift().split(' ');
+// //         }
+// //     console.log(r.join(', '));
+// // }
+// // arrayModifier(['23 -2 321 87 42 90 -123', 'swap 1 3', 'swap 3 6', 'swap 1 0', 'multiply 1 2', 'multiply 2 1', 'decrease','pesho', 'end']);
+
+// let numsAsString = ["5", "23", "45", "-2"];
+// let nums = [5, 23, 45, -2];
+
+// let r = mapToNumber(nums);
+// console.log(r);
 
 // function mapToNumber(arr){
 //     let result = [];
-//     for( element of arr){
-//         result.push(Number(element));
-//     }
 
+//     for (element of arr) { 
+//     result.push(increment(element));
+//     }
 //     return result;
 // }
 
-// function increment(arr){
-    
-    
-//     return x+1;
-  
+// function increment(x){
+//     return x + 1;
 // }
-// console.log(x)
 
+// function swap(){
 
-// function arrayModifier(arr) {
-
-
-//     let command = arr.shift().split(' ');
-//     while (command !== 'end') {
-//         if (command[0] == 'swap') {
-//         [r[command[1]], r[command[2]]] = [r[command[2]], r[command[1]]];
-//         } else if (command[0] == 'multiply'){ 
-//         r[command[1]] *= r[command[2]];
-//         } else if (command[0] == 'decrease') 
-//         for (let i = 0; i < r.length; i++) { 
-//             r[i] -= 1;
-//         }
-//             if (command == 'end') { break; }
-//             command = arr.shift().split(' ');
-//         }
-//     console.log(r.join(', '));
 // }
-// arrayModifier(['23 -2 321 87 42 90 -123', 'swap 1 3', 'swap 3 6', 'swap 1 0', 'multiply 1 2', 'multiply 2 1', 'decrease','pesho', 'end']);
 
-let numsAsString = ["5", "23", "45", "-2"];
-let nums = [5, 23, 45, -2];
+// function multiply(){
 
-let r = mapToNumber(nums);
-console.log(r);
+// }
 
-function mapToNumber(arr){
-    let result = [];
+// function decrease(){
 
-    for (element of arr) { 
-    result.push(increment(element));
-    }
-    return result;
-}
-
-function increment(x){
-    return x + 1;
-}
-
-function swap(){
-
-}
-
-function multiply(){
-
-}
-
-function decrease(){
-
-}
+// }
