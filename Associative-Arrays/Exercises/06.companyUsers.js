1cbc5b455debe5a9652fc10ee824411c23fcaa12
kadodibg <kadodi@gmail.com>
2022-07-10 14:24:18 +0300

    Up

--- Associative-Arrays/Exercises/06.companyUsers.js
+++ Associative-Arrays/Exercises/06.companyUsers.js
@@ -1,20 +1,18 @@
-function companyUsers(input){
+function companyUsers(input) {
     let companies = {};
-    
-    for(let line of input){
-        let [company, id] = line.split(' -> ');
-        if(!companies.hasOwnProperty(company)){
+    for (const elem of input) {
+        let [company, id] = elem.split(' -> ');
+        if (!companies.hasOwnProperty(company)) {
             companies[company] = new Set();
         }
         companies[company].add(id);
     }
     let sorted = Object.entries(companies);
-    sorted.sort((a,b) => { a[0].localeCompare(b[0])});
-
-    for(let el of sorted){
-        console.log(el[0]);
-        for(let num of el[1]){
-            console.log(`-- ${num}`);
+    sorted.sort((a, b) => a[0].localeCompare(b[0]));
+    for (let elem of sorted) {
+        console.log(elem[0]);
+        for (let number of elem[1]) {
+            console.log(`-- ${number}`);
         }
     }
 }
